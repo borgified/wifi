da8e15c9b8633547524c5b71de702410ea533087
borgified <borgified@gmail.com>
2018-04-24 23:49:08 -0700

    fix indentation

--- wifi.sh
+++ wifi.sh
@@ -6,28 +6,28 @@ mkdir -p ${HOME}/.interfaces
 
 function list_profiles {
   ACTIVE=`ls /etc/network/interfaces.d`
-	if [ "$ACTIVE" == "" ]; then
+  if [ "$ACTIVE" == "" ]; then
     echo "no profile set as active"
-	elif [ -L "/etc/network/interfaces.d/$ACTIVE" ]; then
-		for profile in `ls ${HOME}/.interfaces`; do
-		  if [ "$profile" == "$ACTIVE" ]; then
-			  echo "(active) $profile"
-			else
-		    echo "         $profile"
-		  fi
-		done
-	fi
+  elif [ -L "/etc/network/interfaces.d/$ACTIVE" ]; then
+    for profile in `ls ${HOME}/.interfaces`; do
+      if [ "$profile" == "$ACTIVE" ]; then
+        echo "(active) $profile"
+      else
+        echo "         $profile"
+      fi
+    done
+  fi
 }
 
 function switch_profile {
   PROFILE=$1
-	if [ -z "$PROFILE" ]; then
-	  echo "use requires another argument (pick a profile)"
-	  exit 1
-	fi
+  if [ -z "$PROFILE" ]; then
+    echo "use requires another argument (pick a profile)"
+    exit 1
+  fi
 
   #see if PROFILE is valid
-  
+
   FOUND=$(ls ${HOME}/.interfaces | grep "$PROFILE" | wc -l)
   if [[ $FOUND == 0 ]]; then
     echo "$PROFILE: unknown profile"
@@ -37,25 +37,25 @@ function switch_profile {
     exit 1
   fi
 
-	ACTIVE=`ls /etc/network/interfaces.d`
-	if [ "$ACTIVE" == "$PROFILE" ]; then
-	  echo "$PROFILE is already active"
+  ACTIVE=`ls /etc/network/interfaces.d`
+  if [ "$ACTIVE" == "$PROFILE" ]; then
+    echo "$PROFILE is already active"
   else
     for profile in `ls /etc/network/interfaces.d`; do
       sudo rm "/etc/network/interfaces.d/$profile"
     done
-		sudo ln -s ${HOME}/.interfaces/${PROFILE} /etc/network/interfaces.d/${PROFILE}
-	fi
+    sudo ln -s ${HOME}/.interfaces/${PROFILE} /etc/network/interfaces.d/${PROFILE}
+  fi
 }
 
 function cleanup {
   for profile in `ls /etc/network/interfaces.d`; do
     #test for broken symlinks
-	  if [[ -L "/etc/network/interfaces.d/$profile" ]] && \
-       [[ ! -a "/etc/network/interfaces.d/$profile" ]]; then
-      sudo rm "/etc/network/interfaces.d/$profile"
-    fi
-  done
+    if [[ -L "/etc/network/interfaces.d/$profile" ]] && \
+      [[ ! -a "/etc/network/interfaces.d/$profile" ]]; then
+    sudo rm "/etc/network/interfaces.d/$profile"
+  fi
+done
 }
 
 function usage {
@@ -78,14 +78,14 @@ function usage {
 if [[ $# -eq 0 ]]; then
   cleanup
   list_profiles
-	exit 0
+  exit 0
 fi
 
 case "$1" in
   use) switch_profile $2
-     ;;
+    ;;
   -h|--help) usage
-     ;;
+    ;;
   *) echo "invalid option"
-     ;;
+    ;;
 esac
